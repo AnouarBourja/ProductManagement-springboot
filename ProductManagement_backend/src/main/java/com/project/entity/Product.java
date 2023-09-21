bf40e10ec2f19d94933a8baf772a21c694c0ad79
AnouarBourja <anwarbourja@gmail.com>
2023-09-21 11:04:17 +0100

    added Product entity (backend)

---
+++ ProductManagement_backend/src/main/java/com/project/entity/Product.java
@@ -0,0 +1,38 @@
+package com.project.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProduct;
+
+    private String nameProduct;
+
+    private Double priceProduct;
+
+    private Date creationDate;
+
+
+
+    public Product(String nameProduct, Double priceProduct, Date creationDate)
+    {
+        super();
+        this.nameProduct = nameProduct;
+        this.priceProduct = priceProduct;
+        this.creationDate = creationDate;
+    }
+
+
+}
