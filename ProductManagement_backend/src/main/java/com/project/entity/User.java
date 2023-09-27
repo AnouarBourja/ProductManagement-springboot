d10cfee765319c00274b62af2cb936b367f67b18
AnouarBourja <anwarbourja@gmail.com>
2023-09-27 12:19:30 +0100

    added User and Role entities (backend)

---
+++ ProductManagement_backend/src/main/java/com/project/entity/User.java
@@ -0,0 +1,32 @@
+package com.project.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User
+{
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long user_id;
+
+    @Column(unique=true)
+    private String username;
+
+    private String password;
+
+    private Boolean enabled;
+
+    @ManyToMany(cascade=CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name="user_role",joinColumns = @JoinColumn(name="user_id") ,
+            inverseJoinColumns = @JoinColumn(name="role_id"))
+    private List<Role> roles;
+
+}
