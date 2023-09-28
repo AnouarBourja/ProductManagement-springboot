92b2b2609c966a99f82ff11c884d495cc21cd91b
AnouarBourja <anwarbourja@gmail.com>
2023-09-28 11:17:04 +0100

    added UserDetailsService implementation (backend)

---
+++ ProductManagement_backend/src/main/java/com/project/security/MyUserDetailsService.java
@@ -0,0 +1,44 @@
+package com.project.security;
+
+import com.project.entity.User;
+import com.project.service.UserService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService
+{
+    UserService userService;
+
+    public MyUserDetailsService(UserService userService)
+    {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+        User user = userService.findUserByUsername(username);
+
+        if (user==null) throw new
+                UsernameNotFoundException("User not found !");
+
+        List<GrantedAuthority> auths = new ArrayList<>();
+
+        user.getRoles().forEach(role -> {
+            GrantedAuthority auhority = new SimpleGrantedAuthority(role.getRole());
+            auths.add(auhority);
+        });
+
+        return new org.springframework.security.core.
+                userdetails.User(user.getUsername(),user.getPassword(),auths);
+    }
+    
+}
